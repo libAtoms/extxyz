b875d9de0741f3a4680dfea785d9ef49d7bf1dcf
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2021-02-16 18:37:29 -0500

    Add test for backslash escaped backslash in quoted string

--- create_kv_tests.py
+++ create_kv_tests.py
@@ -70,6 +70,7 @@ for c in [chr(i) for i in range(32, 127)]:
 print_config(f'tests_quotedstring.xyz', 'qs="'+all_quoted_str+'"')
 print_config(f'tests_quotedstring.xyz', 'qs="line one\\nline two"')
 print_config(f'tests_quotedstring_"a".xyz', 'qs="\\"a\\""')
+print_config(f'tests_quotedstring_a\\b.xyz', 'qs="a\\\\b"')
 
 # backward compat one-d arrays
 for seps in [ ('"', '"'), ('{', '}'), ('[', ']') ]:
