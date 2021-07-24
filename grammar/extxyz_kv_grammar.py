34d445de6c628719843aa2b28be5b03c97bd8884
James Kermode <james.kermode@gmail.com>
2021-07-24 20:29:12 +0100

    3rd time lucky

--- grammar/extxyz_kv_grammar.py
+++ grammar/extxyz_kv_grammar.py
@@ -118,6 +118,6 @@ def write_grammar(dest_dir):
         fhdr.write('#define STRING_FMT ' + to_C_str(string_fmt) + '\n')
         fhdr.write('#define BOOL_FMT ' + to_C_str(bool_fmt) + '\n')
 
-if __name___ == '__main__':
+if __name__ == '__main__':
     import os
     write_grammar(os.getcwd())
